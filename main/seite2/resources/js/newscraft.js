36f3a3e534c18ff028d0f14cb47c8a4477b93ae2
Vilkaz <admin@vilkas.de>
2016-06-23 11:32:11 +0200

    fehlt noch Profil

--- main/seite2/resources/js/newscraft.js
+++ main/seite2/resources/js/newscraft.js
@@ -4,6 +4,50 @@ function toogleNewscraftItems() {
     if (newsCraftItems.hasClass('hide')){
         newsCraftItems.removeClass('hide')
     } else {
+        hideTextEditor()
+    }
+}
+
+function toogleWriteComment() {
+    hideAudiocontrolls();
+    hideVideoControlls();
+    var newsCraftItems =  jQuery('#writeTextarea');
+    if (newsCraftItems.hasClass('hide')){
+        newsCraftItems.removeClass('hide')
+    } else {
         newsCraftItems.addClass('hide');
     }
 }
+function toogleUploadMP3() {
+    hideTextEditor();
+    hideVideoControlls();
+    var audioControlls =  jQuery('#ncuploadmp3');
+    if (audioControlls.hasClass('hide')){
+        audioControlls.removeClass('hide')
+    } else {
+        hideAudiocontrolls();
+    }
+}
+function toogleUploadVideo() {
+    hideTextEditor();
+    hideAudiocontrolls();
+    var videoControlls =  jQuery('#ncVideoUpload');
+    if (videoControlls.hasClass('hide')){
+        videoControlls.removeClass('hide')
+    } else {
+        hideVideoControlls();
+    }
+}
+
+function hideTextEditor(){
+     jQuery('#writeTextarea').addClass('hide');
+}
+
+function hideAudiocontrolls(){
+     jQuery('#ncuploadmp3').addClass('hide');
+}
+
+function hideVideoControlls(){
+     jQuery('#ncVideoUpload').addClass('hide');
+}
+
